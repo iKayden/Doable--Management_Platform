59b1a04392e6ab58077478a9f2c66d5f1b0c08b0
ikayden <ikyden23@gmail.com>
2022-11-24 06:57:14 -0800

    add schema and seeds

---
+++ backend/db/seeds.sql
@@ -0,0 +1,176 @@
+INSERT INTO
+users(name, avatar, email, password)
+VALUES
+    (
+      'Mario',
+      'https://www.shutterstock.com/image-vector/cool-monkey-sunglasses-baseball-caps-600w-1289802334.jpg',
+      'mario@nintendo.com',
+      'test'
+    ),
+    (
+      'Luigi',
+      'https://www.shutterstock.com/image-vector/monkey-sunglasses-cool-gorilla-600w-1063902011.jpg',
+      'luigi@nintendo.com',
+      'test'
+    ),
+    (
+      'Princess',
+      'https://www.shutterstock.com/image-vector/hipster-santa-claus-premium-vector-600w-1849912864.jpg',
+      'peach@nintendo.com',
+      'test'
+    ),
+    (
+      'Princess',
+      'https://www.shutterstock.com/image-vector/lion-head-christmas-vector-illustration-600w-1548286121.jpg',
+      'daisy@nintendo.com',
+      'test'
+    ),
+    (
+      'Donkey',
+      'https://www.shutterstock.com/image-vector/graphical-portrait-monkey-santa-claus-600w-1089090518.jpg',
+      'donkey@nintendo.com',
+      'test'
+    );
+-- // YYYY-MM-DD hh:mm:ss
+INSERT INTO
+projects(name, description, start_date, expected_end_date)
+VALUES
+    (
+      'alpha',
+      'alpha project, first and best',
+      '2021-10-12 01:00:00'
+      '2025-12-01 03:14:07'
+     ),
+    (
+      'beta',
+      'beta project, first and best',
+      '2021-11-22 03:00:00'
+      '2025-12-01 09:14:07'
+     ),
+    (
+      'delta',
+      'delta project, first and best',
+      '2020-02-12 01:00:00'
+      '2027-12-01 03:14:07'
+     ),
+    (
+      'Project 99',
+      'Project in progress',
+      '2022-01-20 01:00:00'
+      '2023-01-01 12:14:07'
+     ),
+    (
+      'Project 54',
+      'Urgent project due',
+      '2022-06-12 09:00:00'
+      '2022-12-31 05:00:07'
+     );
+
+INSERT INTO
+tasks(name, description, status, deadline, user_id, project_id)
+VALUES
+    (
+      'Clean up',
+      'it is dirty, time to clean up',
+      'IN-PROGRESS',
+      '2025-11-08 05:00:00',
+      '1',
+      '1'
+    ),
+    (
+      'Create database',
+      'Create tables in SQL',
+      'IN-PROGRESS',
+      '2023-01-08 09:00:00',
+      '2',
+      '2'
+    ),
+    (
+      'Design new logo',
+      'Need logo for new project',
+      'IN-PROGRESS',
+      '2022-12-31 05:00:00',
+      '2',
+      '3'
+    ),
+    (
+      'Draft up letter',
+      'Response for letter required',
+      'IN-PROGRESS',
+      '2023-05-30 12:30:00',
+      '3',
+      '3'
+    ),
+    (
+      'Buy office supplies',
+      'Ran out of paper',
+      'IN-PROGRESS',
+      '2024-02-25 08:30:00',
+      '4',
+      '4'
+    ),
+    (
+      'Review notes',
+      'Meeting notes need to be revised',
+      'IN-PROGRESS',
+      '2025-03-01 06:15:00',
+      '5',
+      '5'
+    )
+
+INSERT INTO
+tasks(name, description, status, deadline, completion_time user_id, project_id)
+VALUES
+    (
+      'Pick Up Food',
+      'Me hungry, me want to eat a lot',
+      'COMPLETED',
+      '2022-11-08 05:00:00',
+      '2022-11-08 04:00:00',
+      '1',
+      '2'
+    ),
+    (
+      'Conquer the world',
+      'Those who have the courage to conquer it are made free and those who are conquered by it are made to suffer until they have the courage to defeat it, or death takes them.',
+      'COMPLETED',
+      '2020-01-08 05:00:00',
+      '2021-06-08 04:00:00',
+      '1',
+      '2'
+    )
+INSERT INTO
+project_users(user_id, project_id)
+VALUES
+    (
+      1,
+      2
+    ),
+    (
+      2,
+      2
+    ),
+    (
+      3,
+      1
+    ),
+    (
+      4,
+      1
+    ),
+    (
+      2,
+      2
+    ),
+    (
+      1,
+      1
+    ),
+    (
+      4,
+      2
+    ),
+    (
+      4,
+      5
+    )
--- backend/db/tables.sql
+++ backend/db/tables.sql
@@ -1,16 +1,42 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    avatar VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
-DROP TABLE IF EXISTS posts CASCADE;
 
-CREATE TABLE posts(
+DROP TABLE IF EXISTS projects CASCADE;
+
+CREATE TABLE projects(
     id SERIAL PRIMARY KEY NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    content TEXT,
-    user_id INTEGER REFERENCES users(id)
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expected_end_date DATE NOT NULL, -- Ask Mentor
+    completion_time TIMESTAMP
 );
+
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TYPE IF EXISTS TASKSTATUS CASCADE;
+CREATE TYPE TASKSTATUS AS ENUM ('TO-DO','IN-PROGRESS','COMPLETED');
+CREATE TABLE tasks(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    status TASKSTATUS DEFAULT 'TO-DO',
+    deadline DATETIME NOT NULL,
+    completion_time TIMESTAMP,
+    user_id INTEGER REFERENCES users(id),
+    project_id INTEGER REFERENCES projects(id)
+);
+
+
+DROP TABLE IF EXISTS project_users CASCADE;
+
+CREATE TABLE project_users(
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    project_id INTEGER REFERENCES projects(id)
+)
