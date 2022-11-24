1d677e8d28cbbdf1faca628635f548c213e18645
ikayden <ikyden23@gmail.com>
2022-11-24 11:55:02 -0800

    keep work on project route

--- client/src/hooks/useApplicationData.jsx
+++ client/src/hooks/useApplicationData.jsx
@@ -33,6 +33,31 @@ const useApplicationData = () => {
     state,
     dispatch,
   };
+  const [state, dispatch] = useReducer(dataReducer, {
+    users: [],
+    loading: true,
+  });
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/api/users',
+    })
+      .then(({
+        data
+      }) => {
+        console.log(data);
+        dispatch({
+          type: SET_USERS,
+          users: data
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return {
+    state,
+    dispatch,
+  };
 };
 
 export default useApplicationData;
