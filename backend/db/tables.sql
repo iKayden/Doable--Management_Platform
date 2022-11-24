f1656ea8a6c3b919348feb09ed2032178f34a216
ikayden <ikyden23@gmail.com>
2022-11-24 05:22:59 -0800

    initial commit

---
+++ backend/db/tables.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE posts(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    user_id INTEGER REFERENCES users(id)
+);
