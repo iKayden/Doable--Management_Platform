5c6e683d6ca77d761b9909de054016d074940459
lyjeileen <lyjeileen@gmail.com>
2022-11-25 15:37:50 -0800

    Add session

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ const usersRouter = require('./routes/users');
 const db = require('./db');
 
 const app = express();
+
+const session = require('express-session');
+app.use(express.json());
+app.use(
+  session({ secret: 'my-secret', resave: false, saveUninitialized: true })
+);
+
 // Ask mentor (Add to the top of the server)
 const dbHelpers = require('./helpers/dbHelpers')(db);
 
@@ -27,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/api/users', usersRouter(dbHelpers));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
