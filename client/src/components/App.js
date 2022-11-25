b6d86d5ce999a320a58b298d26eeaaf9c842bf62
ikayden <ikyden23@gmail.com>
2022-11-24 18:36:23 -0800

    Stabilizing the main branch after big merge.[bugs]

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,46 +6,14 @@ import UserList from "./UserList";
 import ProjectList from "./ProjectList";
 
 const App = () => {
-  const {
-    state,
-    dispatch,
-    deleteProject,
-    createProject,
-    editProject
-  } = useApplicationData();
 
-  // const userList = state.users.map((user) => (
-  //   <li key={user.id} >
-  //     <p>
-  //       {user.name}
-  //     </p>
-  //     <img className="user--avatar" src={user.avatar} alt="avatar" />
-  //     <p>
-  //       {user.email}
-  //     </p>
-  //     <hr />
-  //   </li>
-  // ));
-  // const projectList = state.projects.map((project) => ( //name, description, start_date, expected_end_date
-  //   <li key={project.id} >
-  //     <p>
-  //       {project.name}
-  //     </p>
-  //     <p>
-  //       {project.description}
-  //     </p>
-  //     <p>
-  //       {project.start_date}
-  //     </p>
-  //     <p>
-  //       {project.expected_end_date}
-  //     </p>
-  //     <button onClick={() => { deleteProject(project.id); }}>Delete</button>
-  //     {/* <button onClick={() => { editProject(project.id); }}>Edit</button>  DO AN ID PAGE AND SET IT UP THERE*/}
-  //     <hr />
-  //   </li>
-  // ));
-
+  // const {
+  //   state,
+  //   dispatch,
+  //   deleteProject,
+  //   createProject,
+  //   editProject
+  // } = useApplicationData();
 
 
   const [cookie, setCookie] = useState();
@@ -54,15 +22,6 @@ const App = () => {
   // if (!cookie) {
   //   return <Login setCookie={setCookie} />;
   //  } //
-  //   <form
-  //   autoComplete="off"
-  //   onSubmit={(e) => {
-  //     e.preventDefault();
-  //     createProject(project);
-  //   }} >
-  //   <input type="text" name="name" placeholder="Enter Project Name" value={project.name} onChange={(event) => setProject((prev) => ({ ...prev, name: event.target.value }))} />
-  //   <button type="submit">Add New Project</button>
-  // </form>
 
   return (<div className="App" >
     <h1> Users </h1>
