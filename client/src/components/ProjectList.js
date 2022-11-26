c23f69201f43440dbe16b9b7af304caad3d5f030
lyjeileen <lyjeileen@gmail.com>
2022-11-25 21:04:02 -0800

    Add context

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,13 +1,37 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 import './ProjectList.css';
-import useApplicationData from '../hooks/useApplicationData';
+import {
+  useApplicationState,
+  useApplicationDispatch,
+} from '../hooks/useApplicationData';
+import { SET_PROJECTS } from '../reducer/data_reducer';
 import ProjectListItem from './ProjectListItem';
 
 export default function ProjectList() {
-  const { state, createProject } = useApplicationData();
-  const projectList = state.projects.map((project) => {
+  const { projects } = useApplicationState();
+  const dispatch = useApplicationDispatch();
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/api/projects',
+    })
+      .then(({ data }) => {
+        dispatch({
+          type: SET_PROJECTS,
+          projects: data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }, [dispatch]);
+
+  const projectList = projects.map((project) => {
     return (
       <ProjectListItem
         key={project.id}
+        id={project.id}
         name={project.name}
         start_date={project.start_date}
         expected_end_date={project.expected_end_date}
@@ -16,7 +40,7 @@ export default function ProjectList() {
     );
   });
   return (
-    <table class="table table-light table-striped">
+    <table className="table table-light table-striped">
       <thead>
         <tr>
           <th scope="col">Project</th>
--- client/src/reducer/data_reducer.js
+++ client/src/reducer/data_reducer.js
@@ -14,20 +14,21 @@ const dataReducer = (state, action) => {
       return {
         ...state,
         projects: action.projects,
-        loading: false
+        loading: false,
       };
     case REMOVE_PROJECT:
       return {
         ...state,
-        projects: state.projects.filter((project) => project.id !== action.id)
+        projects: state.projects.filter((project) => project.id !== action.id),
       };
     case ADD_PROJECT:
       return {
         ...state,
-        projects: [action.project, ...state.projects]
+        projects: [action.project, ...state.projects],
       };
     default:
       return state;
   }
 };
+
 export default dataReducer;
