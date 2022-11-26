fdf01a9bf38b1edca138dabfa48ad099630816ff
lyjeileen <lyjeileen@gmail.com>
2022-11-26 09:12:32 -0800

    Add onClick function for project name

--- client/src/components/ProjectListItem.jsx
+++ client/src/components/ProjectListItem.jsx
@@ -1,11 +1,18 @@
 import { useApplicationDispatch } from '../hooks/useApplicationData';
 import { deleteProject } from '../api/project';
+import { getTasksForProject } from '../api/task';
 
 export default function ProjectListItem(props) {
   const dispatch = useApplicationDispatch();
   return (
     <tr key={props.id}>
-      <th>{props.name}</th>
+      <th
+        onClick={() => {
+          getTasksForProject(dispatch, props.id);
+        }}
+      >
+        {props.name}
+      </th>
       <th>{props.start_date}</th>
       <th>{props.expected_end_date}</th>
       <th>{props.description}</th>
