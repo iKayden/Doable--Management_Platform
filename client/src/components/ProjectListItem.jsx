f7562e1efbb11baf4b81fbd72452565976da3273
lyjeileen <lyjeileen@gmail.com>
2022-11-29 16:08:44 -0800

    Update ProjectListItem.jsx

--- client/src/components/ProjectListItem.jsx
+++ client/src/components/ProjectListItem.jsx
@@ -8,16 +8,13 @@ export default function ProjectListItem(props) {
   // extract current path (url)
   const dispatch = useApplicationDispatch();
   return (
-
     <tr key={props.id}>
       <th
         onClick={() => {
           dispatch({ type: SET_PROJECT, id: props.id });
         }}
       >
-        <Link to={`projects/${props.id}/tasks`}>
-          {props.name}
-        </Link>
+        <Link to={`/projects/${props.id}/tasks`}>{props.name}</Link>
       </th>
       <th>{props.start_date}</th>
       <th>{props.expected_end_date}</th>
