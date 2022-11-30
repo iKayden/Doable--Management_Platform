5159de9078f40c3ea714a130cd1cbc8e9d0b126f
lyjeileen <lyjeileen@gmail.com>
2022-11-29 21:04:57 -0800

    Add nav bar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,34 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 import './App.css';
 import Login from './Login';
 import ProjectList from './ProjectList';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './Home';
-import { ApplicationContext, defaultState, useApplicationState } from '../hooks/useApplicationData';
+import { ApplicationContext, defaultState } from '../hooks/useApplicationData';
 import { useReducer } from 'react';
 import dataReducer from '../reducer/data_reducer';
 import TaskList from './TaskList';
 
 const App = () => {
-  // const { taskId, tasks } = useApplicationState();
-
   const [state, dispatch] = useReducer(dataReducer, defaultState);
 
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <Home />,
     },
     {
-      path: "/login",
+      path: '/login',
       element: <Login />,
     },
     {
-      path: "/projects",
+      path: '/projects',
       element: <ProjectList />,
     },
     {
@@ -39,6 +38,17 @@ const App = () => {
 
   return (
     <ApplicationContext.Provider value={{ state, dispatch }}>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Doable</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/projects">Project History</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/ask">Ask Us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <RouterProvider router={router} />
     </ApplicationContext.Provider>
   );
