ff6f51d478c78af05af9fcbbccf2068040327470
lyjeileen <lyjeileen@gmail.com>
2022-11-29 16:06:53 -0800

    Add project users list for creating a task

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -1,43 +1,59 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import { createTask } from '../api/task';
-import { useApplicationDispatch, useApplicationState } from '../hooks/useApplicationData';
+import { getUsersByProjectId } from '../api/user';
+import {
+  useApplicationDispatch,
+  useApplicationState,
+} from '../hooks/useApplicationData';
 
 export default function TaskForm(props) {
   const { id } = useParams();
   const dispatch = useApplicationDispatch();
-  const [task, setTask] = useState({ name: '', description: '', assigned_user_id: null, project_id: id });
-  const { users } = useApplicationState();
+  const [task, setTask] = useState({
+    name: '',
+    description: '',
+    assigned_user_id: null,
+    project_id: id,
+  });
   const [selectedUser, setSelectedUser] = useState({});
+  const [projectUsers, setProjectUsers] = useState([]);
 
+  useEffect(() => {
+    getUsersByProjectId(id).then((users) => {
+      setProjectUsers(users);
+    });
+  }, [id]);
+  console.log(projectUsers);
   // Function to display all users for select in Add New Task
-  const userList = users.map(user =>
-    <option value={user.id} key={`${user.id}${user.name}`} >{user.email}</option>
-  );
+  const userList = projectUsers.map((user) => (
+    <option value={user.id} key={`${user.id}${user.name}`}>
+      {user.email}
+    </option>
+  ));
 
   const newAssignedUser = (selectedUser, task) => {
     return { ...task, assigned_user_id: selectedUser };
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     createTask(dispatch, newAssignedUser(selectedUser, task));
-    setTask((prev) => ({ ...prev, description: "" }));
-    setTask((prev) => ({ ...prev, name: "" }));
+    setTask((prev) => ({ ...prev, description: '' }));
+    setTask((prev) => ({ ...prev, name: '' }));
   };
 
   return (
-    <form
-      autoComplete="off"
-      onSubmit={handleSubmit}
-    >
+    <form autoComplete="off" onSubmit={handleSubmit}>
       <div className="container">
-        <div className='d-inline'>
-
-          <label htmlFor="name" className='m-3'>Task Name:</label>
+        <div className="d-inline">
+          <label htmlFor="name" className="m-3">
+            Task Name:
+          </label>
           <input
-            id='name'
-            className='m-3'
+            id="name"
+            className="m-3"
             type="text"
             name="name"
             placeholder="Enter Task Name"
@@ -47,14 +63,15 @@ export default function TaskForm(props) {
             }
           />
         </div>
-        <div className='d-inline'>
-
-          <label htmlFor="description" className='m-3'>Task Description:</label>
+        <div className="d-inline">
+          <label htmlFor="description" className="m-3">
+            Task Description:
+          </label>
           <input
-            id='description'
+            id="description"
             type="text"
             name="description"
-            className='m-3'
+            className="m-3"
             placeholder="Enter Task description"
             value={task.description}
             onChange={(event) =>
@@ -62,18 +79,23 @@ export default function TaskForm(props) {
             }
           />
         </div>
-        <div className='d-inline'>
-          <label htmlFor="assigned_user_id" className='m-3'>Assign a user:</label>
+        <div className="d-inline">
+          <label htmlFor="assigned_user_id" className="m-3">
+            Assign a user:
+          </label>
           <select
             name="assigned_user_id"
             id="assigned_user_id"
             onChange={(e) => {
               setSelectedUser(e.target.value);
-            }}>
-            <option value="" >--Please assign a user--</option>
+            }}
+          >
+            <option value="">--Please assign a user--</option>
             {userList}
           </select>
-          <button type="submit" className="btn btn-warning m-3">Add</button>
+          <button type="submit" className="btn btn-warning m-3">
+            Add
+          </button>
         </div>
       </div>
     </form>
