c39819d7d6068815555b91e2ba6bf79f958e1202
lyjeileen <lyjeileen@gmail.com>
2022-12-01 13:41:12 -0800

    Set time zone for nodeJS

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+process.env.TZ = 'Pacific Standard Time';
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -56,12 +57,12 @@ app.use('/api/projects', projectRouter(dbHelpers));
 app.use('/api/tasks', tasksRouter(dbHelpers));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -70,6 +71,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
