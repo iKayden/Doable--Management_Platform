428c29fb3a778f253ace1daf272efa2a5d77d6d1
lyjeileen <lyjeileen@gmail.com>
2022-12-03 16:21:08 -0800

    Edit error message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -14,17 +14,22 @@ router.post('/login', async (req, res) => {
   const data = await db.query(findUserByEmail, [req.body.email]);
 
   if (data.rows.length === 0) {
-    res.status(400).send({ message: 'Email or password is wrong' });
+    res
+      .status(400)
+      .send({
+        message:
+          'The email address or password you entered was incorrect. Please try again.',
+      });
     return;
   }
 
   const user = data.rows[0];
   if (user.password !== req.body.password) {
-    return res.status(403).send
-      ({
-        status: 'error',
-        message: 'Email or password is wrong',
-      });
+    return res.status(403).send({
+      status: 'error',
+      message:
+        'The email address or password you entered was incorrect. Please try again.',
+    });
   }
   res.json(user);
   req.session.user_id = user.id;
