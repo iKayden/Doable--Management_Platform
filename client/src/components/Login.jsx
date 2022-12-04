47344aa75a2a14c02e280018d4b1a3abd954aeb3
lyjeileen <lyjeileen@gmail.com>
2022-12-03 16:21:31 -0800

    Edit css of input area

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 import './Login.css';
 
 export default function Login(props) {
@@ -10,7 +11,7 @@ export default function Login(props) {
   const [error, setError] = useState();
   const navigate = useNavigate();
 
-  const handleSubmit = function(event) {
+  const handleSubmit = function (event) {
     event.preventDefault();
     fetch('/login', {
       method: 'POST',
@@ -31,41 +32,45 @@ export default function Login(props) {
         localStorage.setItem('userName', data.name);
         localStorage.setItem('userAvatar', data.avatar);
         props.setUser(data.name);
-        navigate("/");
+        navigate('/');
         setError('');
       })
       .catch((err) => {
         if (err) {
           console.log(err);
-          err.json()
-            .then((json) => {
-              setError(json.message);
-            });
+          err.json().then((json) => {
+            setError(json.message);
+          });
         }
       });
   };
 
   return (
     <div className="login">
-      <h3>
-        Please login or <a href={url}>create a new account</a>.
-      </h3>
+      <img className="logo-login" src="/doable_logo_new.png" alt="logo" />
+      <h3>Welcome Back!</h3>
+      <div>{error}</div>
       <form onSubmit={handleSubmit}>
-        <div>{error}</div>
-        <label>
-          <p>Email</p>
-          <input
+        <Form.Group controlId="dob">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
             type="text"
+            name="email"
+            placeholder="Enter email address"
+            value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
-        </label>
-        <label>
-          <p>Password</p>
-          <input
-            type="password"
+        </Form.Group>
+        <Form.Group controlId="dob">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="text"
+            name="password"
+            placeholder="Enter password"
+            value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-        </label>
+        </Form.Group>
         <div>
           <button type="submit">Submit</button>
         </div>
