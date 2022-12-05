4f25e7c00d77e1a003f3f65ce157150e3d9b37a6
ikayden <ikyden23@gmail.com>
2022-12-05 00:20:04 -0800

    add new pictures. More css styles

--- client/src/components/AboutUsPage.jsx
+++ client/src/components/AboutUsPage.jsx
@@ -35,9 +35,9 @@ const Box = ({ content }) => {
 
 function First() {
   return (
-    <section>
+    <section className='about-us__section'>
       <h1>Hello everyone!</h1>
-      <p>This is our website</p>
+      <p>This was demonstration of our website</p>
       <p>We hope you enjoyed our project and presentation!</p>
     </section>
   );
@@ -45,35 +45,41 @@ function First() {
 
 function Second() {
   return (
-    <section className="box section__images">
+    <section className="box about-us__section">
       <h2>This is the dev stack we've used to develop it</h2>
-      <img
-        src="https://github.com/iKayden/doable/blob/kayden/about-ut-page/client/public/express_js.png?raw=true"
-        alt="express-logo"
-      ></img>
-      <img
-        src="https://github.com/iKayden/doable/blob/kayden/about-ut-page/client/public/JavaScript-logo.png?raw=true"
-        alt="js-logo"
-      ></img>
-      <img
-        src="https://github.com/iKayden/doable/blob/kayden/about-ut-page/client/public/node-js-ico.jpg?raw=true"
-        alt="nodejs-logo"
-      ></img>
-      <img
-        src="https://res.cloudinary.com/practicaldev/image/fetch/s--qo_Wp38Z--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/e0nl7ziy1la7bpwj7rsp.png"
-        alt="react-logo"
-      ></img>
+      <div className="section__images">
+        <img
+          src="https://ih1.redbubble.net/image.438908244.6144/st,small,507x507-pad,600x600,f8f8f8.u2.jpg"
+          alt="express-logo"
+        ></img>
+        <img
+          src="https://github.com/iKayden/doable/blob/kayden/about-ut-page/client/public/JavaScript-logo.png?raw=true"
+          alt="js-logo"
+        ></img>
+        <img
+          src="https://the-guild.dev/blog-assets/nodejs-esm/nodejs_logo.png"
+          alt="nodejs-logo"
+        ></img>
+        <img
+          src="https://ionicframework.com/docs/icons/logo-react-icon.png"
+          alt="react-logo"
+        ></img>
+        <img
+          src="https://www.influxdata.com/wp-content/uploads/PostgreSQL-logo.jpg"
+          alt="postgresql-logo"
+        ></img>
+      </div>
     </section>
   );
 }
 
 function Third() {
   return (
-    <section>
+    <section className='about-us__section'>
       <h2>This is our amazing team</h2>
       <img
         className="about-us_group-photo"
-        src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-20936.jpg?w=2000"
+        src="https://res.cloudinary.com/kaydenukr/image/upload/c_scale,h_1800,w_2400/v1670265748/pictures/bestteam_atlwus.jpg"
         alt="our-team"
       ></img>
     </section>
@@ -82,63 +88,80 @@ function Third() {
 
 function Fourth() {
   return (
-    <section className="about-us__our-stories hidden">
-      <h3>Our stories</h3>
+    <section className="about-us__our-stories about-us__section">
       <h2>Eileen</h2>
-      <img
-        className="about-us__first-picture"
-        src="https://images.squarespace-cdn.com/content/v1/5ca268ee01232c1d4131debd/1635466037445-MZYSB3WNAHHLAV3KGVBE/unsplash-image-yMSecCHsIBc.jpg?format=1500w"
-        alt="eileen"
-      ></img>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
-        officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
-        laborum eius natus quos, tempora nam corrupti inventore libero
-        distinctio fugiat.
-      </p>
+      <div className="flex-wrapper__our-stories">
+        <img
+          className="about-us__first-picture"
+          src="https://res.cloudinary.com/kaydenukr/image/upload/c_scale,h_880,w_500/v1670266958/pictures/eileen_vuscdv_ba2245.jpg"
+          alt="eileen"
+        ></img>
+        <p className='about-us__text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
+          officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
+          laborum eius natus quos, tempora nam corrupti inventore libero
+          distinctio fugiat.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
+          officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
+          laborum eius natus quos, tempora nam corrupti inventore libero
+          distinctio fugiat.
+        </p>
+      </div>
     </section>
   );
 }
 
 function Fifth() {
   return (
-    <section className="about-us__our-stories">
+    <section className="about-us__our-stories about-us__section">
       <h2>Rosanna</h2>
-      <img
-        className="about-us__second-picture"
-        src="https://i.pinimg.com/474x/71/11/5b/71115b2c2b4823d55818c52fa75ab125.jpg"
-        alt="rosanna"
-      ></img>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
-        officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
-        laborum eius natus quos, tempora nam corrupti inventore libero
-        distinctio fugiat.
-      </p>
-    </section>
+      <div className="flex-wrapper__our-stories">
+        <img
+          className="about-us__second-picture"
+          src="https://res.cloudinary.com/kaydenukr/image/upload/c_scale,w_500/v1670267447/pictures/rosanna_fiuioq_e42cc3.jpg"
+          alt="rosanna"
+        ></img>
+        <p className='about-us__text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
+          officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
+          laborum eius natus quos, tempora nam corrupti inventore libero
+          distinctio fugiat.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
+          officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
+          laborum eius natus quos, tempora nam corrupti inventore libero
+          distinctio fugiat.
+        </p>
+      </div>
+    </section >
   );
 }
 function Sixth() {
   return (
-    <section className="about-us__our-stories hidden">
+    <section className="about-us__our-stories about-us__section">
       <h2>Kayden</h2>
-      <img
-        className="about-us__third-picture"
-        src="https://i.pinimg.com/originals/6c/51/63/6c5163a050ab57480410ea9a56a3ca93.jpg"
-        alt="kayden"
-      ></img>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
-        officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
-        laborum eius natus quos, tempora nam corrupti inventore libero
-        distinctio fugiat.
-      </p>
+      <div className="flex-wrapper__our-stories">
+        <img
+          className="about-us__third-picture"
+          src="https://res.cloudinary.com/kaydenukr/image/upload/c_scale,w_500/v1670267315/pictures/kayden_jhr6uk_4f4282.jpg"
+          alt="kayden"
+        ></img>
+        <p className='about-us__text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
+          officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
+          laborum eius natus quos, tempora nam corrupti inventore libero
+          distinctio fugiat.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
+          officia, expedita vitae illum eum laboriosam! Ut obcaecati pariatur
+          laborum eius natus quos, tempora nam corrupti inventore libero
+          distinctio fugiat.
+        </p>
+      </div>
     </section>
   );
 }
 function Seventh() {
   return (
-    <section className="hidden">
+    <section className="about-us__section">
       <h1>Thank you for your time and attention</h1>
       <img
         className="about-us__closeup-picture"
@@ -152,10 +175,13 @@ function Seventh() {
 export default function AboutUsPage() {
   return (
     <>
-      <div className="inside-about-us__wrapper">
+      <div className="inside-about-us__wrapper about-us__section">
         <Box content={<First />} />
         <Box content={<Second />} />
         <Box content={<Third />} />
+        <Box content={<h3
+          className='about-us__our-stories about-us__section'
+        >Our stories</h3>} />
         <Box content={<Fourth />} />
         <Box content={<Fifth />} />
         <Box content={<Sixth />} />
